800306d6b5fd93d5a0579d82b68f0357222fa79f
Ayrton <ayrton.huenchual@usach.cl>
2022-01-22 05:31:13 -0300

    Primera modificacion del backend

---
+++ Backend/src/main/java/cl/tbd/Lab2/models/HabilidadMongo.java
@@ -0,0 +1,46 @@
+package cl.tbd.Lab2.models;
+
+import org.springframework.data.annotation.Id;
+
+public class HabilidadMongo {
+    @Id
+    private String id;
+    private String nombre;
+    private String description;
+    private String codigo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+
+    }
+
+}
